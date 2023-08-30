1cab9d4be7e19cd0ee1287e30b225adf8c347e2d
alex <alexkist@gmx.de>
2023-08-30 17:21:49 +0200

    Media player patches

--- __init__.py
+++ __init__.py
@@ -12,7 +12,8 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Keep hass happy."""
     _LOGGER.warn("Installing monkey patches")
-    from . import async_upnp_client
+    #from . import async_upnp_client
+    from . import media_player
     _LOGGER.warn("Monkey patches installed")
 
     return True
---
+++ media_player.py
@@ -0,0 +1,42 @@
+"""Monkey patches for async_upnp_client."""
+
+import logging
+import re
+from typing import Mapping, Optional, Tuple
+
+from homeassistant.components.dlna_dmr.media_player import DlnaDmrEntity
+from async_upnp_client.exceptions import UpnpXmlParseError, UpnpError, UpnpResponseError
+
+_LOGGER = logging.getLogger(__name__)
+
+
+
+async def async_update_patched(self) -> None:
+        """Retrieve the latest data."""
+        if not self._device:
+            if not self.poll_availability:
+                return
+            try:
+                await self._device_connect(self.location)
+            except UpnpError:
+                return
+
+        assert self._device is not None
+
+        try:
+            do_ping = self.poll_availability or self.check_available
+            await self._device.async_update(do_ping=do_ping)
+        except UpnpXmlParseError as err:
+            #Device seems to send bad XML. So does Denon Ceol Piccolo when not in Media Server mode
+            _LOGGER.debug("Device NOT unavailable: %r", err)
+            return
+        except UpnpError as err:
+            _LOGGER.debug("Device unavailable: %r", err)
+            await self._device_disconnect()
+            return
+        finally:
+            self.check_available = False
+
+DlnaDmrEntity._async_update_orig = DlnaDmrEntity.async_update
+DlnaDmrEntity.async_update=async_update_patched
+_LOGGER.warn("Monkey patches installed for async_update installed")
